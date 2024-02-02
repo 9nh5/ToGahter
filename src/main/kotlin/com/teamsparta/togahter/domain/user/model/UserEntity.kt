a2c34176b6e4a91932cc7144cbed2ccc816df77d
9nh5 <koon6756@naver.com>
2024-02-02 10:34:44 +0900

    feat: Add User

--- src/main/kotlin/com/teamsparta/togahter/domain/user/model/UserEntity.kt
+++ src/main/kotlin/com/teamsparta/togahter/domain/user/model/UserEntity.kt
@@ -20,22 +20,6 @@ class UserEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
-
-    companion object {
-        fun userData(request: SignUpRequest, encoder: PasswordEncoder) = UserEntity(
-            ProfileEntity(
-                name = request.name,
-                phone = request.phone,
-                age = request.age,
-                region = request.region,
-                email = request.email,
-                password = encoder.encode(request.password),
-                nickname = request.nickname,
-                interest = request.interest,
-                introduction = request.introduction
-            ), role = UserRole.USER
-        )
-    }
 }
 
 fun UserEntity.signUp(): UserResponse {
